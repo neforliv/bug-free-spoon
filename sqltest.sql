d8208e02b04eab8882119b8ca887ff75b9a667ec
a <a>
2018-01-03 12:04:23 -0600

    Adding test file

---
+++ sqltest.sql
@@ -0,0 +1,10 @@
+select * from sandbox.asd_tablefix
+--- used to be different, let's see what it looks like 2
+
+---NEW LINES
+
+---YAY
+
+--let's add another line and see if my hunch about how it works when you commit locally vs push to origin is correct
+
+---time testqwe
